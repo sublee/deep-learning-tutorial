1577069e171285897bbf7f9f8598e6cf0f79cb0a
Hans Lee <hans.lee@kakaobrain.com>
2019-01-28 17:01:33 +0900

    Make Cifar224 follow Cifar's interface

--- skeleton/datasets/cifar224.py
+++ skeleton/datasets/cifar224.py
@@ -14,40 +14,34 @@ CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
 
 class Cifar224:
     @staticmethod
-    def datasets(num_classes=10, cv_ratio=0.2, cv_seed=None, root='./data'):
+    def sets(batch_size, num_classes=10, root='./data'):
         assert num_classes in [10, 100]
 
         dataset = tv.datasets.CIFAR10 if num_classes == 10 else tv.datasets.CIFAR100
-        data_shape = [(3, 224, 224), (1,)]
+        data_shape = [(batch_size, 3, 224, 224), (batch_size,)]
 
         transform_train = tv.transforms.Compose([
+            tv.transforms.Pad(4, padding_mode='reflect'),
             tv.transforms.RandomResizedCrop(224),
             tv.transforms.RandomHorizontalFlip(),
             tv.transforms.ToTensor(),
             tv.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
         ])
         transform_valid = tv.transforms.Compose([
-            tv.transforms.Resize(256),
-            tv.transforms.CenterCrop(224),
+            tv.transforms.Resize(224),
             tv.transforms.ToTensor(),
             tv.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
         ])
         train_set = dataset(root=root, train=True, download=True, transform=transform_train)
         test_set = dataset(root=root, train=False, download=True, transform=transform_valid)
 
-        spliter = skorch.dataset.CVSplit(cv=cv_ratio, stratified=True, random_state=cv_seed)
-        train_set, valid_set = spliter(train_set, y=np.array(train_set.train_labels, dtype=np.int8))
-
-        return train_set, valid_set, test_set, data_shape
+        return train_set, test_set, data_shape
 
     @staticmethod
-    def loader(batch_size, num_classes=10, cv_ratio=0.2, cv_seed=None, root='./data', num_workers=8):
-        train_set, valid_set, test_set, data_shape = Cifar224.datasets(num_classes, cv_ratio, cv_seed, root)
-
-        data_shape = [(batch_size,) + shape for shape in data_shape]
+    def loader(batch_size, num_classes=10, root='./data', num_workers=8):
+        train_set, test_set, data_shape = Cifar.sets(batch_size, num_classes=num_classes, root=root)
 
         train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True, drop_last=True)
-        valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True, drop_last=False)
         test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True, drop_last=False)
 
-        return train_loader, valid_loader, test_loader, data_shape
+        return train_loader, test_loader, data_shape
